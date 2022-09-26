ae194ae4e6f84610f3cfcfda56389ce087b0b19a
Dustin Kaban <dustin@voodoo.io>
2022-09-26 11:09:41 -0600

    first project commit

---
+++ Assets/Code/Installers/GameInstaller.cs
@@ -0,0 +1,27 @@
+using Code.Managers;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Installers
+{
+    public class GameInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject playerPrefab;
+        private SceneContext _sceneContext;
+        
+        public override void InstallBindings()
+        {
+            _sceneContext = GetComponent<SceneContext>();
+            _sceneContext.PostInstall += InitGame;
+            
+            Container.Bind<IsoWorldManager>().AsSingle();
+        }
+
+        private void InitGame()
+        {
+            Container.InstantiatePrefab(playerPrefab);
+            
+            _sceneContext.PostInstall -= InitGame;
+        }
+    }
+}
---
+++ Assets/Code/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+using System;
+using Code.Managers;
+using UnityEngine;
+using Zenject;
+
+namespace Code.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidBody;
+        [Inject] private IsoWorldManager _isoWorldManager;
+        private Vector3 _input;
+        private PlayerData _playerData;
+
+        private void Awake()
+        {
+            _playerData = Resources.Load<PlayerData>("PlayerData");
+        }
+
+        private void Update()
+        {
+            GatherInput();
+            Look();
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+        }
+
+        private void GatherInput()
+        {
+            _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        }
+
+        private void Look()
+        {
+            if (_input == Vector3.zero) return;
+            
+            var skewedInput = _isoWorldManager.ToIso(_input);
+            var relative = (transform.position + skewedInput) - transform.position;
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _playerData.turnSpeed * Time.deltaTime);
+        }
+
+        private void Move()
+        {
+            _rigidBody.MovePosition(
+                transform.position + (transform.forward * _input.magnitude) 
+                * _playerData.movementSpeed * Time.deltaTime);
+        }
+    }
+}
